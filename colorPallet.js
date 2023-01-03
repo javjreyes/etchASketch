d625189afbfc6254ca87de91a1c276687f72da45
JavierReyes <javjreyes@gmail.com>
2023-01-03 10:26:21 -0600

    colorPallet comments

--- colorPallet.js
+++ colorPallet.js
@@ -1,10 +1,12 @@
 export class colorPallet{
+    //manually add color pallets contain hex codes
     constructor(){
         this.vibrant=["0044FF","F1D302","0FFF95","FF3C38","E637BF"];
         this.pastels=["DABFFF","907AD6","F9C784","B7FFD8","7FDEFF"];
         this.monochrom=["CEE5f2","ACCBE1","7C98B3","7C98B3","637081","536B78"];
     }
 
+    //accept color pallet name and return array with color names
     getPallet(pallet){
         switch(pallet){
             case "vibrant":
